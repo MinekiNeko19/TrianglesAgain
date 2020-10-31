622fbe21016140d673ff81f1fdacfdcb5bfe2714
Sarah Leong <sleong20@stuy.edu>
2020-10-31 01:03:41 -0400

    Tested getPerimeter and getArea

--- PointTester.java
+++ PointTester.java
@@ -7,11 +7,21 @@ public class PointTester {
         Point e = new Point(2,3);
         Point f = new Point(2,1);
 
-        System.out.println(a.distanceTo(b));
-        System.out.println(a.distanceTo(d));
-        System.out.println(a.distanceTo(c));
+        Triangle scalene = new Triangle(a,f,c);
+        Triangle isosceles = new Triangle(0,0,3,0,0,3);
+        Triangle equilateral = new Triangle(0.1,0.1,-1.9,0.1,-0.9,-1.632051);
 
-        System.out.println(a.equals(b));
-        System.out.println(a.equals(c));
+        // System.out.println(a.distanceTo(b));
+        // System.out.println(a.distanceTo(d));
+        // System.out.println(a.distanceTo(c));
+
+        // System.out.println(a.equals(b));
+        // System.out.println(a.equals(c));
+
+        System.out.println(scalene.getPerimeter());
+        System.out.println(isosceles.getPerimeter());
+
+        System.out.println(scalene.getArea());
+        System.out.println(isosceles.getArea());
     }
 }
